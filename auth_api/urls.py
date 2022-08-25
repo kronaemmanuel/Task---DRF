d006fc6a967c1b7db027b129961591480fc41c05
Krona Emmanuel <kronaemmanuel@gmail.com>
2022-08-25 14:37:52 +0000

    Add Register User API

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from auth_api.views import UserList
+from auth_api.views import UserList, UserRegister, api_root
 
 urlpatterns = format_suffix_patterns([
-    path('users/', UserList.as_view(), name='users-list')
+    path('users/', api_root, name='users'),
+    path('users/list/', UserList.as_view(), name='users-list'),
+    path('users/register/', UserRegister.as_view(), name='users-register')
 ])
--- auth_api/views.py
+++ auth_api/views.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-from auth_api.serializers import UserSerializer
+from auth_api.serializers import UserRegisterSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users/list': reverse('users-list', request=request, format=format),
+        'users/register': reverse('users-register', request=request, format=format)
+    })
 
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserRegister(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
